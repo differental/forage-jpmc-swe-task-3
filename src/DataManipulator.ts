20de155816f117e0b4da80c7c5e19972d24d5c0f
Brian Chen <enderchen004@outlook.com>
2024-01-09 09:08:35 +0000

    Task 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,31 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  trigger_alert: number | undefined,
+  upper_bound: number,
+  lower_bound: number,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    const price_ABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const price_DEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const ratio = price_ABC / price_DEF;
+    const UpperBound = 1.05;
+    const LowerBound = 0.95;
+    return {
+      price_abc: price_ABC,
+      price_def: price_DEF,
+      ratio: ratio,
+      trigger_alert: (ratio>UpperBound || ratio<LowerBound) ? ratio : undefined,
+      upper_bound: UpperBound,
+      lower_bound: LowerBound,
+      timestamp: serverResponds[0].timestamp>serverResponds[1].timestamp?serverResponds[0].timestamp:serverResponds[1].timestamp,
+    };
   }
 }
